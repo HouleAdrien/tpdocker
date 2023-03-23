79f579edcfb9349afd0b9b75b287829de41d973e
Houle Adrien <adrienhoule01@gmail.com>
2023-03-23 16:47:15 +0100

    added endpoint for testing database connection

--- index.js
+++ index.js
@@ -16,11 +16,27 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello, World!');
 });
 
-app.get('/add', async (req, res) => {
+app.post('/add', async (req, res) => {
   const name = 'toto';
   const result = await pool.query('INSERT INTO names (name) VALUES ($1)', [name]);
   res.send(`Name ${name} added to database`);
 });
+
+
+app.get('/db', async (req, res) => {
+  try {
+    await pool.query('SELECT 1 + 1');
+    const result = await pool.query('SELECT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_name = $1)', ['names']);
+    if (result.rows[0].exists) {
+      res.send('Connected to database and table exists');
+    } else {
+      res.send('Connected to database but table does not exist');
+    }
+  } catch (err) {
+    res.status(500).send(`Error connecting to database: ${err}`);
+  }
+});
+
 
 app.get('/get', async (req, res) => {
   const result = await pool.query('SELECT * FROM names');
